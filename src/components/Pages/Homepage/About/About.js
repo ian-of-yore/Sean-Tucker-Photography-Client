00d974aafada43fd2c7fdfd8daa770c0253d7102
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-11-10 01:10:16 +0600

    homepage about section done

--- src/components/Pages/Homepage/About/About.js
+++ src/components/Pages/Homepage/About/About.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import img from '../../../../assets/sean.jfif';
 
 const About = () => {
     return (
-        <div style={{ minHeight: "30vh" }}>
-            <p className='text-center text-xl font-bold my-10'>This is the about section</p>
+        <div className='w-10/12 mx-auto grid grid-cols-10 shadow-xl my-20'>
+            <div className='col-span-2'>
+                <img src={img} alt="" style={{ width: "90%", height: "40vh" }} />
+            </div>
+            <div className='col-span-8 pt-3'>
+                <h3 className='text-3xl font-serif mb-1'>Sean is a Photographer, Filmmaker, Author and Speaker based in the UK.</h3>
+                <p className='pr-10 font-sans'>
+                    He has a roboust career in his grasp. At a young age he worked with multiple reknowed NGO's across the African Continent
+                    to provide sanctuary and care for wild, endengered animals. At some point he devoted himself towards religious works and served
+                    as a priest under the catholic church. During this period he discovered his passion and talent for photography. Due to some personal
+                    reasons he severed his role with the church, but remained in touch with his fond of photography throughout the years. He has worked in
+                    various domains of photography including portrait, commercial product photography, solo filmmaking and most important of all
+                    <span className='font-semibold'> street photography.</span> He shares his works on a regular basis on his instragram accout. He has authored a philosophical book for creative
+                    minded people titled, <span className='font-semibold'>"The Meaning in the Making"</span>. One of the main things that distinguishes Sean from others is his way of teaching others
+                    about the ins & outs of photography. It's never limited to just the technical stuffs rathers it's more about helping others finding their
+                    own identity, passion and talents. All this has made his recently opened YouTube channel explode with positive, innovative and creative thinkers.
+
+                </p>
+            </div>
         </div>
     );
 };
--- src/components/Pages/Shared/Footer/Footer.js
+++ src/components/Pages/Shared/Footer/Footer.js
@@ -4,13 +4,13 @@ import { SlSocialTwitter, SlSocialYoutube, SlSocialInstagram } from "react-icons
 const Footer = () => {
     return (
         <footer className="footer flex justify-center md:flex-row flex-col items-center p-4 bg-neutral text-neutral-content">
-            <div className="items-center grid-flow-col ml-10">
-                <p className='text-lg'>Copyright ©seantuck, 2022 - All right reserved</p>
+            <div className="ml-10 pb-1">
+                <p className=''>Copyright ©shooting on the street, 2022 - All right reserved</p>
             </div>
-            <div className="grid-flow-col mr-10 gap-4 md:place-self-center md:justify-self-end">
-                <a href="https://twitter.com/seantuck?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" rel="noopener noreferrer"><SlSocialTwitter className='h-6 w-6'></SlSocialTwitter></a>
-                <a href="https://www.youtube.com/channel/UC_43mQmHwHPTBBqImFrWU3Q" target="_blank" rel="noopener noreferrer"><SlSocialYoutube className='h-6 w-6'></SlSocialYoutube></a>
-                <a href="https://www.instagram.com/seantuck/?hl=en" target="_blank" rel="noopener noreferrer"><SlSocialInstagram className='h-6 w-6'></SlSocialInstagram></a>
+            <div className="flex items-center">
+                <a href="https://twitter.com/seantuck?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" rel="noopener noreferrer"><SlSocialTwitter className='h-4 w-4'></SlSocialTwitter></a>
+                <a href="https://www.youtube.com/channel/UC_43mQmHwHPTBBqImFrWU3Q" target="_blank" rel="noopener noreferrer"><SlSocialYoutube className='h-4 w-4'></SlSocialYoutube></a>
+                <a href="https://www.instagram.com/seantuck/?hl=en" target="_blank" rel="noopener noreferrer"><SlSocialInstagram className='h-4 w-4'></SlSocialInstagram></a>
             </div>
         </footer>
     );
